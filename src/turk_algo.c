d5b08720a697b59353290f9ef65a6b4b212888cd
m-bartos <m.bartos96@seznam.cz>
2024-01-09 11:48:26 +0100

    Add: turk_algo.c

---
+++ src/turk_algo.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   turk_algo.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/09 11:35:50 by mbartos           #+#    #+#             */
+/*   Updated: 2024/01/09 11:48:07 by mbartos          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	turk_algo(t_node stack_a, t_node stack_b)
+{
+	// if number of ints is 4 -> push 1 to stack_b
+	// else (number of ints >= 5) -> push 2 to stack_b
+
+	// pushing to stack_b according to algo, until num of ints in stack_a == 3
+
+	// start pushing back from stack_b to stack_a according to algo
+	// rotate stack_a until lowest number at the top
+}
